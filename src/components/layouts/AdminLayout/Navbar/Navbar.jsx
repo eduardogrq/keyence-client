da1bde29295a1d4627ba1264a2f782cb2e07c042
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-28 11:00:44 -0600

    fix: solving navbar issue

--- src/components/layouts/AdminLayout/Navbar/Navbar.jsx
+++ src/components/layouts/AdminLayout/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = ({ turnToHidden }) => {
                     </div>
 
                     {/* Dropdown */}
-                    <div className={`absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
+                    <div className={`hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
                         ${isActive ? 'd-block' : 'd-none'}`}
                         role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                         <div className="py-1" role="none">
