243b373a7efdbbad5c908fc9a69cb6edfddeddc0
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-27 22:48:47 -0600

    feature: creating loading status into users route to wait for users response

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -5,9 +5,11 @@ import AdminLayout from "../../components/layouts/AdminLayout";
 const Users = () => {
 
   const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const urlAPI = 'https://keyence-backend-eva8.onrender.com/users'
 
   const getUsers = async () => {
+    setIsLoading(true)
     try {
       const response = await fetch(urlAPI)
       const data = await response.json()
@@ -15,6 +17,8 @@ const Users = () => {
       setUsers(data)
     } catch (err) {
       console.error(err.message)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -50,23 +54,48 @@ const Users = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {users.map(user => (
-                    <tr key={`tr-${user.userId}-${user.userName}`} class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.userId}</td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {user.userName}
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {moment(user.date).format('DD/MM/YYYY HH:mm:ss')}
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
-                      </td>
-                    </tr>
-                  ))}
+                  {isLoading ?
+                    users.map(user => (
+                      <tr key={`tr-${user.userId}-${user.userName}`} class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.userId}</td>
+                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {user.userName}
+                        </td>
+                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {moment(user.date).format('DD/MM/YYYY HH:mm:ss')}
+                        </td>
+                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
+                        </td>
+                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
+                        </td>
+                      </tr>
+                    ))
+                    :
+                    (
+                      // Creating array of 4 length only to not repeat 4 times html code
+                      new Array(6).fill().map((_, i) => (
+                        <tr key={`tr-${i}`} class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          </td>
+                        </tr>
+                      ))
+                    )
+                  }
                 </tbody>
               </table>
             </div>
