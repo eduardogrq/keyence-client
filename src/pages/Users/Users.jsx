562d7e513c423ae8f69bd7468d92bc9a899497e5
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-28 00:08:15 -0600

    fix: solving datetime issues

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import moment from 'moment';
+import moment from 'moment-timezone';
 import AdminLayout from "../../components/layouts/AdminLayout";
 
-const Users = () => {
+moment.tz.setDefault('UTC');
 
+const Users = () => {
   const [users, setUsers] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const urlAPI = 'https://keyence-backend-eva8.onrender.com/users'
@@ -29,26 +30,26 @@ const Users = () => {
   return (
     <AdminLayout title="Usuarios">
 
-      <div class="flex flex-col animate__animated animate__fadeIn">
-        <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-              <table class="min-w-full">
-                <thead class="bg-gray-200 border-b">
+      <div className="flex flex-col animate__animated animate__fadeIn">
+        <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-gray-200 border-b">
                   <tr>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       User ID
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Username
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                      Date
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Fecha
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Punch in
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Punch out
                     </th>
                   </tr>
@@ -56,19 +57,19 @@ const Users = () => {
                 <tbody>
                   {!isLoading ?
                     users.map(user => (
-                      <tr key={`tr-${user.userId}-${user.userName}`} class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.userId}</td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <tr key={`tr-${user.userId}-${user.userName}`} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.userId}</td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {user.userName}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {moment(user.date).format('DD/MM/YYYY HH:mm:ss')}
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {moment(user.date).format('DD/MM/YYYY')}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {moment(user.punchIn).format("HH:mm:ss A")}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {moment(user.punchIn).format('DD/MM/YYYY HH:mm:ss')}
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {moment(user.punchOut).format("HH:mm:ss A")}
                         </td>
                       </tr>
                     ))
@@ -76,21 +77,21 @@ const Users = () => {
                     (
                       // Creating array of 4 length only to not repeat 4 times html code
                       new Array(6).fill().map((_, i) => (
-                        <tr key={`tr-${i}`} class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                          <td class="px-6 py-4">
-                            <div class="rounded-full bg-slate-300 h-2 w-1/3 animate-pulse"></div>
+                        <tr key={`tr-${i}`} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                          <td className="px-6 py-4">
+                            <div className="rounded-full bg-slate-300 h-2 w-1/3 animate-pulse"></div>
                           </td>
-                          <td class="px-6 py-4">
-                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          <td className="px-6 py-4">
+                            <div className="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
                           </td>
-                          <td class="px-6 py-4">
-                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          <td className="px-6 py-4">
+                            <div className="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
                           </td>
-                          <td class="px-6 py-4">
-                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          <td className="px-6 py-4">
+                            <div className="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
                           </td>
-                          <td class="px-6 py-4">
-                            <div class="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
+                          <td className="px-6 py-4">
+                            <div className="rounded-full bg-slate-300 h-2 w-2/3 animate-pulse"></div>
                           </td>
                         </tr>
                       ))
